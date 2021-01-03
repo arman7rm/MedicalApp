18b202ab0bc2bf61283b2448d27d5c291831c121
arman <arman97rm@gmail.com>
2021-01-03 16:36:07 -0500

    booking model created

---
+++ Domain/Models/Booking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    class Booking
+    {
+        public int BookingId { get; set; }
+        public int DoctorId { get; set; }
+        public int UserId { get; set;  }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -2,10 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Domain.Interfaces
+namespace Domain.Models
 {
-    interface IBookingRepo
+    class User
     {
-
     }
 }
