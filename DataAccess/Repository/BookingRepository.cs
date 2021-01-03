483094186e09ee171971d28047ddfe6c13769428
arman <arman97rm@gmail.com>
2021-01-03 18:22:12 -0500

    finished booking repo

--- DataAccess/Repository/BookingRepository.cs
+++ DataAccess/Repository/BookingRepository.cs
@@ -18,27 +18,69 @@ namespace DataAccess.Repository
         }
         public int Add(d.Booking newBooking)
         {
-            throw new NotImplementedException();
+            var entity = new Booking
+            {
+                Date = newBooking.Date,
+                DoctorId = newBooking.DoctorId,
+                PatientId = newBooking.UserId,
+                Description = newBooking.Description
+            };
+            _context.Bookings.Add(entity);
+            _context.SaveChanges();
+            return entity.BookingId;
         }
 
         public void Delete(int Id)
         {
-            throw new NotImplementedException();
+            var booking = _context.Bookings.Find(Id);
+            if(booking == null)
+            {
+                throw new ArgumentNullException();
+            }
+            _context.Bookings.Remove(booking);
+            _context.SaveChanges();
         }
 
         public IEnumerable<d.Booking> GetByDoctor(int DoctorId)
         {
-            throw new NotImplementedException();
+            var entities = _context.Bookings
+                .Where(b => b.DoctorId == DoctorId);
+            return entities.Select(e => new d.Booking
+            {
+                BookingId = e.BookingId,
+                DoctorId = e.DoctorId,
+                Description = e.Description,
+                Date = e.Date,
+                UserId = e.PatientId
+            });
         }
 
         public IEnumerable<d.Booking> GetByUser(int UserId)
         {
-            throw new NotImplementedException();
+            var entities = _context.Bookings
+                .Where(b => b.PatientId == UserId);
+            return entities.Select(e => new d.Booking
+            {
+                BookingId = e.BookingId,
+                DoctorId = e.DoctorId,
+                Description = e.Description,
+                Date = e.Date,
+                UserId = e.PatientId
+            });
         }
 
         public void Update(d.Booking newBooking)
         {
-            throw new NotImplementedException();
+            var entity = _context.Bookings.Find(newBooking.BookingId);
+            if (entity == null)
+            {
+                throw new ArgumentNullException();
+            }
+            entity.Description = newBooking.Description;
+            entity.Date = DateTime.Now;
+            entity.DoctorId = newBooking.DoctorId;
+            entity.PatientId = newBooking.UserId;
+            _context.SaveChanges();
         }
     }
 }
