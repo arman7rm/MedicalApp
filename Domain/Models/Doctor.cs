7229c3d148fa8a5e923235e8298dfa419f5aade4
arman <arman97rm@gmail.com>
2021-01-03 16:39:58 -0500

    doctor model created

--- Domain/Models/Doctor.cs
+++ Domain/Models/Doctor.cs
@@ -6,5 +6,19 @@ namespace Domain.Models
 {
     class Doctor
     {
+        public int DoctorId { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string email { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Bio { get; set; }
+        public int Exp { get; set; }
+        public double Fee { get; set; }
+        public double Rating { get; set; }
+        public string Phone { get; set; }
+        public List<Booking> Appointments = new List<Booking>();
     }
 }
